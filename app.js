21e5034266933846ca5fb0b5d41e319bca1daf4c
g218suna <su.ikumi0408@gmial.com>
2022-02-18 12:17:11 +0900

    remove node_modules

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-
+const http = require('http');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
@@ -22,6 +22,12 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+const httpServer = http.Server(app);
+httpServer.listen(80, () => {
+  process.setuid(1000);
+  console.log('Server Started');
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
